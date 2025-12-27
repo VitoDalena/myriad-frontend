e65e2097ae5ce1c205a57a32a8251e333be832ee
Vito Dalena <vito.dalena@gmail.com>
2025-12-27 11:53:11 +0100

    December events

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -10,7 +10,8 @@ import { RouterModule } from '@angular/router';
 export class IndexComponent {
   chapters = [
     { id: 0 },
-    { id: 1 }
+    { id: 1 },
+    { id: '1-b'}
   ];
 
   scroll(el: HTMLElement) {
